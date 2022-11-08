2bce47f011612be182ccc89bec1d9b5672b459ea
Yoonji <86295076+minyoonji95@users.noreply.github.com>
2022-11-08 11:14:12 +0500

    fix: add appendFile

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,7 +9,10 @@ const input = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 console.log('Введите пожалуйста текст: \n');
 stdin.on('data', text => {
     if (text.toString().trim() == 'exit') exit();
-    input.write(text.toString());
+    fs.appendFile(path.join(__dirname, 'text.txt'), `${text}`, err => {
+        if (err) throw err
+    }
+    )
 })
 process.on('exit', () => stdout.write('До новых встреч!\n'));
 process.on('SIGINT', () => process.exit());
