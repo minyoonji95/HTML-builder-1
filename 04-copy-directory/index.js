c7b646b6bb2171e1432b1385209e127e4a5acc19
Yoonji <86295076+minyoonji95@users.noreply.github.com>
2022-11-08 11:27:44 +0500

    fix: changing errors for correct copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,26 @@
-
 const fs = require('fs');
 const path = require('path');
 
 fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-if (err) throw err
+    if (err) {
+        console.log(err);
+    }
 });
     fs.readdir(path.join(__dirname, './files'), (err, files) => {
-        if (err) throw err;
+        if (err) {
+          console.log(err)
+        }
         else {
           console.log("\nCurrent directory filenames:");
           files.forEach(file => {
               fs.copyFile((path.join(__dirname, `./files/${file}`)), (path.join(__dirname, `./files-copy/${file}`)), err => {
-                if (err) throw err
+                if (err) {
+                  console.log(err);
+                }
                 else {
                     console.log('files were copied successfully')
                 }
               })
-                // })
           })
         }
       })
