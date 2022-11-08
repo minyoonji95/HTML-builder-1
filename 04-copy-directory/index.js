f6f62e67fbc7eeb13c3340d1a1b5006ff01f9f5a
Yoonji <86295076+minyoonji95@users.noreply.github.com>
2022-11-08 10:57:28 +0500

    add: create copy-file function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
 
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+if (err) throw err
+});
+    fs.readdir(path.join(__dirname, './files'), (err, files) => {
+        if (err) throw err;
+        else {
+          console.log("\nCurrent directory filenames:");
+          files.forEach(file => {
+              fs.copyFile((path.join(__dirname, `./files/${file}`)), (path.join(__dirname, `./files-copy/${file}`)), err => {
+                if (err) throw err
+                else {
+                    console.log('files were copied successfully')
+                }
+              })
+                // })
+          })
+        }
+      })
